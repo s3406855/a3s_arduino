605c390d2fd95b977b21129ece8fa3135e09fddb
s3406855 <s3406855@student.rmit.edu.au>
2016-08-30 16:46:10 +1000

    So many updates

--- d3b_arduino.ino
+++ d3b_arduino.ino
@@ -2,37 +2,67 @@
  * TO-DO LIST
  * ~~~~~~~~~~
  * 
- * -- Check Atmega32/ousb schematic and double check pin choices. (Note: some hacking of the ousb board will be required to get all the analog inputs needed
- * -- If we're going to do any testing using an actual arduino, specify correct pins for the arduino model being used (comment out Atmega32/ousb specific pins)
+ * -- Check Atmega32/ousb schematic and double check pin choices. (Note: some hacking of the ousb board will be required to get all the analog inputs needed)
  * -- Make sure the logic makes sense, and is what the hardware boys need
  * -- Double check the PID constructor to make sure it's correctly set up (look in the .cpp file that came in the library)
- * -- Go through the code and identify any immediates that can be replaced by variables (will make life easier as we start testing with hardware specific values)
  * 
  */
+
+ /*
+  * PINOUT FOR ARDUINO
+  * 
+  * DIGITAL I/O             ANALOG I
+  * 0   N/A                 A0    N/A
+  * 1   N/A                 A1    ACCELEROMETER
+  * 2   LCD                 A2    K_P
+  * 3   LCD                 A3    K_I
+  * 4   LCD                 A4    K_D
+  * 5   LCD                 A5    REFERENCE / SETPOINT
+  * 6   N/A
+  * 7   STEPPER DIRECTION
+  * 8   STEPPER PWM
+  * 9   N/A
+  * 10  N/A
+  * 11  LCD
+  * 12  LCD
+  * 13  N/A
+  */
+
+#define OSCILLATE // if this line is uncommented, the controller will bypass the PID loop and oscillate slowly from max left to max right
+#define USE_LCD // comment this out if LDC use is NOT needed to save program space / increase efficiency
+
 #include <PID_v1.h>
 
-#define TEST1 // if this line is uncommented, the controller will bypass the PID loop and oscillate slowly from max left to max right
+#ifdef USE_LCD
+#include <LiquidCrystal.h>
+#endif
 
+/*
+ * "CLICKS" - it was mentioned at one point that the shock adjustment might move in clicks of around 15 degrees each.
+ * I don't think the mountain bike shock does this but i'm not sure.
+ * If it turns out that the adjustment moves continuously, all we need to do is set MAX_CLICKS to 400 (720 deg / 1.8 deg)
+ * and set steps_per_click to 1.
+ */
 #define MAX_CLICKS 48 // number of clicks in either direction to get 720 degrees
 
 // holds the current rotation of the motor (one unit = one 'click' on shock)
 volatile int8_t position_count = 0;
 
-// define a couple of pins for ease of reference - these may not be the best choice - pulled them out of a hat
-volatile int8_t direction_pin = 12; // On ousb use PB0, pin 1
-volatile int8_t step_pin = 13; // On ousb use PB1, pin 2
+// define a couple of pins for ease of reference - these should be fine for Arduino but need to be verified for OUSB
+volatile uint8_t direction_pin = 7; // On ousb use PB0, pin 1
+volatile uint8_t step_pin = 8; // On ousb use PB1, pin 2
 
 // Accelerometer input pins
 //volatile int8_t accel_top = A0; // On ousb use PA0, pin 40 - TOP ACCELEROMETER NOT NEEDED - VALUE HARD CODED
-volatile int8_t accel_bot = A1; // On ousb use PA1, pin 39
+volatile uint8_t accel_bot = A1; // On ousb use PA1, pin 39
 
 // Gain set pins
-volatile int8_t k_p_pin = A2; // On ousb use PA2, pin 38
-volatile int8_t k_i_pin = A3; // On ousb use PA3, pin 37
-volatile int8_t k_d_pin = A4; // On ousb use PA4, pin 36
+volatile uint8_t k_p_pin = A2; // On ousb use PA2, pin 38
+volatile uint8_t k_i_pin = A3; // On ousb use PA3, pin 37
+volatile uint8_t k_d_pin = A4; // On ousb use PA4, pin 36
 
 // Reference point set pin
-volatile int8_t ref_pin = A5; // On ousb use PA5, pin 35
+volatile uint8_t ref_pin = A5; // On ousb use PA5, pin 35
 
 // Doubles required for PID library. K values and REF read in during setup() from trimpots
 volatile double K_P, K_I, K_D, REF, PID_IN, PID_OUT;
@@ -41,31 +71,48 @@ volatile double K_P, K_I, K_D, REF, PID_IN, PID_OUT;
 volatile int8_t threshold = 0.5;
 
 // one 'click' on the shock = multiple motor steps - number TBC, assuming approx 8 for now
-volatile int8_t steps_per_click = 8;
+volatile uint8_t steps_per_click = 8;
 
 /*
  * Min and Max delay time sets the min and max stepping speed of the motor
- * I think the arduino library specifies milliseconds but this may be too course for us
- * 1 clock cycle at 12MHz is 83.33 nanoseconds so it should be possible to drill down to a finer delay time
+ * Used to set a delay time between steps in response to a PID calculation
+ * Clock period at 12MHz is 83.33 nanoseconds
  */
-volatile int16_t min_delay = 1; // ms? micros or nanos would be better
-volatile int16_t max_delay = 100;
+volatile uint16_t min_delay = 1000; // microseconds
+volatile uint16_t max_delay = 100000;
 
 // Delay time to control the PWM signal
-volatile int16_t delay_us;
+volatile uint32_t delay_us;
 
 // Creat a PID object
 PID a3s(&PID_IN, &PID_OUT, &REF, K_P, K_I, K_D, DIRECT);
 
+// Accelerometer resting value
+int16_t acc_offset;
+
+/*
+ * LCD for testing / config
+ * Initialize the library with the numbers of the interface pins
+ * NEED TO REASSIGN SOME PINS BECAUSE THERE IS A CLASH WITH PREVIOUSLY DEFINED OUTPUTS
+ */
+#ifdef USE_LCD
+LiquidCrystal lcd( 12, 11, 5, 4, 3, 2 );
+#endif
+
 void setup(void)
 {
-  // set direction pin as output (pin number specified in globals)
-  pinMode(direction_pin, OUTPUT);
+  // DDRs
+  pinMode(direction_pin, OUTPUT); // set direction pin as output (pin number specified in globals)
+  pinMode(step_pin, OUTPUT);  // set stepper signal pin as output (pin number specified in globals)
   
-  // set stepper signal pin as output (pin number specified in globals)
-  pinMode(step_pin, OUTPUT);
-
-  // Set up ADC
+  pinMode(A0, INPUT); // Analog pin to INPUT
+  pinMode(A1, INPUT); // Analog pin to INPUT
+  pinMode(A2, INPUT); // Analog pin to INPUT
+  pinMode(A3, INPUT); // Analog pin to INPUT
+  pinMode(A4, INPUT); // Analog pin to INPUT
+  pinMode(A5, INPUT); // Analog pin to INPUT
+  
+  // Set ADC reference voltage
   analogReference(DEFAULT); // sets upper limit to 5v
 
   // Read gain and ref values
@@ -76,17 +123,63 @@ void setup(void)
 
   //turn the PID on
   a3s.SetMode(AUTOMATIC);
+
+  // Calibrate the accelerometer - find out where it's sitting and determine an offset
+  // Idea - to take 5 or so readings over 5 seconds and take an average, then this result should be the offset
+  // NEW THOUGHT - DO WE EVEN NEED TO DO THIS, OR CAN WE JUST SET THE REFERENCE TO BE EQUAL TO THE RESTING VALUE?
+  for (unsigned short i = 0; i < 5; i++)
+  {
+    acc_offset += analogRead(accel_bot);
+  }
+  acc_offset = acc_offset / 5;
+
+  #ifdef USE_LCD
+  lcd.begin(16, 2); // 16 columns, 2 rows
+  #endif
 }
 
 void loop(void)
 {
-  #ifdef TEST1
-  // write oscillate code here
-  while (1)
-  {
-    
-    //
-  }
+  // if OSC definition at top is not commented out, the following test code will execute forever
+  #ifdef OSCILLATE
+    delay_us = 20000; // 20 milliseconds
+    while (1)
+    {
+      goToMax();
+      goToMin();
+    }
+  #endif
+
+  /*
+   * Ignore LCD unless it is "turned on"
+   */
+  #ifdef USE_LCD
+  String p, i, d, r;
+  
+  p = (K_P / 1024);
+  i = (K_I / 1024);
+  d = (K_D / 1024);
+  r = (REF / 1024);
+
+  lcd.setCursor(0, 0);
+  lcd.print("P=");
+  lcd.setCursor(2, 0);
+  lcd.print(p);
+
+  lcd.setCursor(8, 0);
+  lcd.print("I=");
+  lcd.setCursor(10, 0);
+  lcd.print(i);
+
+  lcd.setCursor(0, 1);
+  lcd.print("D=");
+  lcd.setCursor(2, 1);
+  lcd.print(d);
+
+  lcd.setCursor(8, 1);
+  lcd.print("R=");
+  lcd.setCursor(10, 1);
+  lcd.print(r);
   #endif
   
   // Read accelerometers
@@ -101,41 +194,20 @@ void loop(void)
   {
     if (position_count < MAX_CLICKS) // still below clockwise max?
     {
-      // set direction bit to HIGH
-      digitalWrite(direction_pin, HIGH);
-
-      // now loop as many times as required to make one full 'click' on the shock
-      for (unsigned short i = 0; i < steps_per_click; i++)
-      {
-        digitalWrite(step_pin, HIGH);
-        delayMicroseconds(delay_us);
-        digitalWrite(step_pin, LOW);
-        delayMicroseconds(delay_us);
-      }
+      StepForward();
       /*
-       * Every time this loop finishes, a 'click' will have been achieved on the shock. If more than one click is required, the final delay() in the above
-       * loop will carry over to the next run of this whole routine. There will be an additional delay due to the clock cycles required to restart the loop.
-       * It's probably not a big deal, but we should minimize unnecessary code prior to the loop - ie. keep as much housekeeping as possible in the setup()
-       * routine, and avoid declaring variables in loop().
+       * Every time this function is called, a 'click' will have been achieved on the shock. If more than one click is required, the final delay() in the
+       * function will carry over to the next run of this whole routine. There will be an additional delay due to the clock cycles required to get back to
+       * the function. It's probably not a big deal, but we should minimize unnecessary code prior to the loop - ie. keep as much housekeeping as possible
+       * in the setup() routine, and avoid declaring variables in loop().
        */
-      position_count++;
     }
   }
   else if (PID_OUT < (-1 * threshold)) // pid commands a negative adjustment
   {
     if (position_count > -MAX_CLICKS) // still above ccw max?
     {
-      // set direction bit LOW
-      digitalWrite(direction_pin, LOW);
-
-      for (unsigned short i = 0; i < steps_per_click; i++)
-      {
-        digitalWrite(step_pin, HIGH);
-        delay(delay_us);
-        digitalWrite(step_pin, LOW);
-        delay(delay_us);
-      }
-      position_count--;
+      StepBack();
     }
   }
 
@@ -167,20 +239,7 @@ void goToMax(void)
   // 720 degrees clockwise
   do
   {
-    // do a positive click
-    // set direction bit to HIGH
-    digitalWrite(direction_pin, HIGH);
-
-    // now loop as many times as required to make one full 'click' on the shock
-    for (unsigned short i = 0; i < steps_per_click; i++)
-    {
-      digitalWrite(step_pin, HIGH);
-      delayMicroseconds(delay_us);
-      digitalWrite(step_pin, LOW);
-      delayMicroseconds(delay_us);
-    }
-    // increment position counter
-    position_count++;
+    StepForward();
   } while (position_count < MAX_CLICKS);
 }
 
@@ -189,18 +248,7 @@ void goToMin(void)
   // 720 degrees counterclockwise
   do
   {
-    // set direction bit LOW
-    digitalWrite(direction_pin, LOW);
-  
-    for (unsigned short i = 0; i < steps_per_click; i++)
-    {
-      digitalWrite(step_pin, HIGH);
-      delay(delay_us);
-      digitalWrite(step_pin, LOW);
-      delay(delay_us);
-    }
-    // decrement position counter
-    position_count--;
+    StepBack();
   } while(position_count > (-MAX_CLICKS));
 }
 
@@ -208,37 +256,50 @@ void returnToZero(void)
 {
   if (position_count < 0)
   {
-    // cw steps to get back to zero - set direction bit to HIGH
-    digitalWrite(direction_pin, HIGH);
+    // cw steps to get back to zero
     do
     {
-      for (unsigned short i = 0; i < steps_per_click; i++)
-      {
-        digitalWrite(step_pin, HIGH);
-        delay(delay_us);
-        digitalWrite(step_pin, LOW);
-        delay(delay_us);
-      }
-      // increment position counter
-      position_count++;
+      StepForward();
     } while (position_count < 0);
   }
   else if (position_count > 0)
   {
-    // ccw steps to get back to zero - set direction bit LOW
-    digitalWrite(direction_pin, LOW);
+    // ccw steps to get back to zero
     do
     {
-      for (unsigned short i = 0; i < steps_per_click; i++)
-      {
-        digitalWrite(step_pin, HIGH);
-        delay(delay_us);
-        digitalWrite(step_pin, LOW);
-        delay(delay_us);
-      }
-      // decrement position counter
-      position_count--;
+      StepBack();
     } while (position_count > 0);
   }
+}
+
+void StepForward(void)
+{
+  // set direction bit to HIGH
+  digitalWrite(direction_pin, HIGH);
+  
+  // now loop as many times as required to make one full 'click' on the shock
+  for (unsigned short i = 0; i < steps_per_click; i++)
+  {
+    digitalWrite(step_pin, HIGH);
+    delayMicroseconds(delay_us);
+    digitalWrite(step_pin, LOW);
+    delayMicroseconds(delay_us);
+  }
+  position_count++;
+}
+
+void StepBack(void)
+{
+  // set direction bit LOW
+  digitalWrite(direction_pin, LOW);
+
+  for (unsigned short i = 0; i < steps_per_click; i++)
+  {
+    digitalWrite(step_pin, HIGH);
+    delay(delay_us);
+    digitalWrite(step_pin, LOW);
+    delay(delay_us);
+  }
+  position_count--;
 }
 
